446f050242d2c3e90458defcae63dfba6a90da3d
kaekoko <kekoko.mm@gamil.com>
2020-03-05 16:19:03 +0630

    t

--- config/app.php
+++ config/app.php
@@ -174,6 +174,7 @@ return [
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
+        App\Providers\LaravelBackupPanelServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Intervention\Image\ImageServiceProvider::class,
         Alexusmai\LaravelFileManager\FileManagerServiceProvider::class,
